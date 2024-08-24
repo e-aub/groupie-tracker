3f7061da0161d17c23866846146ec26c2fb14634
yassinerahhaui <yassinerahhaoui12@gmail.com>
2024-08-24 12:34:24 +0100

    build

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM golang:1.22.5
-
-WORKDIR "/project"
-
-COPY . .
-
-EXPOSE 8000
-
-CMD ["./run"]
--- dockerrun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --name groupie_tracker -v $(pwd):/project -dp 8000:8000 groupie_tracker
